148e8dbfe6025ba7a6db685e6f6a62ec49195c58
Shanu Mathew <shanutmathew1703@gmail.com>
2023-06-13 00:41:35 +0530

    Correcting Predict Pipeline

--- app.py
+++ app.py
@@ -2,75 +2,77 @@ import streamlit as st
 import pandas as pd
 
 from src.pipeline.predict_pipeline import PredictPipeline
-
+from src.logger import logging
 st.write('''
     <h1 style="text-align:center;">Wine Quality Prediction App</h1>
     <p style="text-align:center;">This app is used to predict the quality of wines with respect to its chemical composition</p>
 ''', unsafe_allow_html=True)
-
+st1=st.empty()
 # Load the wine quality dataset
 wine_data = pd.read_csv('.\Assets\cleaned_data.csv')
-
 class QualityPredictionApp:
     def input():
         # Split the data into features (X) and target (y)
         X = wine_data.drop('quality', axis=1)
         y = wine_data['quality']
+        k=st1.form(key='my-form')
+        with k:
+            col1,col2,col3=st.columns(3)
+            # Create input widgets for user input
+            with col1:
+                fixed_acidity = st.number_input("Fixed Acidity", float(X['fixed acidity'].min()), float(X['fixed acidity'].max()))
+            with col2:
+                volatile_acidity = st.number_input("Volatile Acidity", float(X['volatile acidity'].min()), float(X['volatile acidity'].max()))
+            with col3:
+                citric_acid = st.number_input("Citric Acid", float(X['citric acid'].min()), float(X['citric acid'].max()))
+            
+            col1,col2,col3=st.columns(3)
+            with col1:
+                residual_sugar = st.number_input("Residual Sugar", float(X['residual sugar'].min()), float(X['residual sugar'].max()))
+            with col2:
+                chlorides = st.number_input("Chlorides", float(X['chlorides'].min()), float(X['chlorides'].max()))
+            with col3:
+                free_sulfur_dioxide = st.number_input("Free Sulfur Dioxide", float(X['free sulfur dioxide'].min()), float(X['free sulfur dioxide'].max()))
 
-        col1,col2,col3=st.columns(3)
-        # Create input widgets for user input
-        with col1:
-            fixed_acidity = st.number_input("Fixed Acidity", float(X['fixed acidity'].min()), float(X['fixed acidity'].max()))
-        with col2:
-            volatile_acidity = st.number_input("Volatile Acidity", float(X['volatile acidity'].min()), float(X['volatile acidity'].max()))
-        with col3:
-            citric_acid = st.number_input("Citric Acid", float(X['citric acid'].min()), float(X['citric acid'].max()))
-        
-        col1,col2,col3=st.columns(3)
-        with col1:
-            residual_sugar = st.number_input("Residual Sugar", float(X['residual sugar'].min()), float(X['residual sugar'].max()))
-        with col2:
-            chlorides = st.number_input("Chlorides", float(X['chlorides'].min()), float(X['chlorides'].max()))
-        with col3:
-            free_sulfur_dioxide = st.number_input("Free Sulfur Dioxide", float(X['free sulfur dioxide'].min()), float(X['free sulfur dioxide'].max()))
-        
-        
-        col1,col2,col3=st.columns(3)
-        with col1:
-            total_sulfur_dioxide = st.number_input("Total Sulfur Dioxide", float(X['total sulfur dioxide'].min()), float(X['total sulfur dioxide'].max()))
-        with col2:
-            density = st.number_input("Density", float(X['density'].min()), float(X['density'].max()))
-        with col3:
-            pH = st.number_input("pH", float(X['pH'].min()), float(X['pH'].max()))
-        
-        col1,col2,col3=st.columns(3)
-        with col1:
-            sulphates = st.number_input("Sulphates", float(X['sulphates'].min()), float(X['sulphates'].max()))
-        with col2:
-            alcohol = st.number_input("Alcohol", float(X['alcohol'].min()), float(X['alcohol'].max()))
-        
-        data={
-            'fixed acidity': [fixed_acidity],
-            'volatile acidity': [volatile_acidity],
-            'citric acid': [citric_acid],
-            'residual sugar': [residual_sugar],
-            'chlorides': [chlorides],
-            'free sulfur dioxide': [free_sulfur_dioxide],
-            'total sulfur dioxide': [total_sulfur_dioxide],
-            'density': [density],
-            'pH': [pH],
-            'sulphates': [sulphates],
-            'alcohol': [alcohol]
-        }
-        features=pd.DataFrame(data,index=[0])
-        return features
+            col1,col2,col3=st.columns(3)
+            with col1:
+                total_sulfur_dioxide = st.number_input("Total Sulfur Dioxide", float(X['total sulfur dioxide'].min()), float(X['total sulfur dioxide'].max()))
+            with col2:
+                density = st.number_input("Density", float(X['density'].min()), float(X['density'].max()))
+            with col3:
+                pH = st.number_input("pH", float(X['pH'].min()), float(X['pH'].max()))
+            
+            col1,col2,col3= st.columns(3)
+            with col1:
+                sulphates = st.number_input("Sulphates", float(X['sulphates'].min()), float(X['sulphates'].max()))
+            with col2:
+                alcohol = st.number_input("Alcohol", float(X['alcohol'].min()), float(X['alcohol'].max()))
+            submit_button = st.form_submit_button(label='Submit')
+            data={
+                'fixed acidity': [fixed_acidity],
+                'volatile acidity': [volatile_acidity],
+                'citric acid': [citric_acid],
+                'residual sugar': [residual_sugar],
+                'chlorides': [chlorides],
+                'free sulfur dioxide': [free_sulfur_dioxide],
+                'total sulfur dioxide': [total_sulfur_dioxide],
+                'density': [density],
+                'pH': [pH],
+                'sulphates': [sulphates],
+                'alcohol': [alcohol]
+            }
+            features=pd.DataFrame(data,index=[0])
+            return features, submit_button
 
     def predict_data(data):
         st.write('Getting Predictions')
         return PredictPipeline.initiate_prediction(PredictPipeline(),data)
 
 if __name__ == '__main__':
-    features=QualityPredictionApp.input()
+
+    #Getting Input from the User
+    features,button=QualityPredictionApp.input()
+
 
     # Create a style for the button
     style = """
@@ -91,13 +93,10 @@ if __name__ == '__main__':
     st.markdown(style, unsafe_allow_html=True)
 
     # Create the button
-    button = st.button("Predict")
     if button:
-        st.empty()
-        print('Button Pressed')
+        logging.info('Button Pressed')
         model_pred,model_prob=QualityPredictionApp.predict_data(features)
-        st.write('Prediction')
-        st.write(model_pred)
+        st.write('The Quality of Wine is:-',model_pred[0])
 
-        st.write('Prediction Probability')
-        st.write(model_prob)
+        st.write('Prediction Probability:-')
+        st.dataframe(model_prob)
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import numpy as np
+import pandas as pd
 
 from src.utils import load_object
 from src.logger import logging
@@ -7,6 +9,7 @@ from src.exception import CustomException
 
 from dataclasses import dataclass
 
+from sklearn.metrics import confusion_matrix as cm
 
 @dataclass
 class PredictPipelineConfig:
@@ -17,14 +20,30 @@ class PredictPipeline:
         self.predict_model=PredictPipelineConfig()
     def initiate_prediction(self,data):
         try:
+
             #Input Data Preprocessing
+            logging.info('Preprocessor Loading for Predict Pipeline')
             preprocessor=load_object(self.predict_model.best_preprocessor_path)
             data_scaled=preprocessor.transform(data)
 
             #Preprocessed Data Prediction
+            logging.info('Model Loading for Predict Pipeline')
             model=load_object(self.predict_model.best_model_path)
+            logging.info('Input Value Prediction')
             model_pred=model.predict(data_scaled)
             model_prob=model.predict_proba(data_scaled)
+            
+            model_prob=pd.DataFrame(model_prob.reshape(1,-1), columns=np.array([3,4,5,6,7,8]))
+
             return model_pred,model_prob
         except Exception as e:
             raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    data=pd.read_csv('Assets/test.csv')
+    data_drp=data.drop('quality',axis=1)
+    logging.info('Predict Pipeline Working')
+    predicted=PredictPipeline.initiate_prediction(PredictPipeline(),data_drp)
+    logging.info('Successful Prediction')
+    actual=data['quality']
+    print(cm(actual,predicted))
